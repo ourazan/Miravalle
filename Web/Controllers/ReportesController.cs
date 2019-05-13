9ef8d09d5ed8703ac8f61b5967f4f4e39ca0203e
ourazan <ourazan05@uan.edu.co>
2019-05-13 00:34:55 -0500

    13052019 modificacion del formulario

---
+++ Web/Controllers/ReportesController.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class ReportesController : Controladores
+    {
+        // GET: Reportes
+        public ActionResult Index()
+        {
+            ViewData["Autenticado"] = ObtenerAutenticado();
+            return View();
+        }
+    }
+}
